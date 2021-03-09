12e01de2baaf0e9b8c74e3513b546464630402b3
Theodora2000 <xsimont@stuba.sk>
2021-03-09 15:50:13 +0100

    1.1-spravne

--- main.c
+++ main.c
@@ -5,19 +5,24 @@ int pole(int a[] ,int velkost){
         a[i] = i;
         //printf("%d ", a[i]);
     }
-
+    printf("%d", sizeof (a));
     return a;
 
 }
 
 int main() {
-    int a[]={};
+
+    //int a[]={};
+
     int velkost = 0;
     scanf(" %d", &velkost);
+    int a[velkost];
 
     pole(a, velkost);
     for(int i=0;i<velkost;i++){
         printf("%d ", a[i]);
     }
+    printf("Sizeof: %d", sizeof (a));
+
     return 0;
 }

72e233d2bc60064b5573cd13502c38da41808aa6
Theodora2000 <xsimont@stuba.sk>
2021-03-12 21:00:57 +0100

    5.1

--- main.c
+++ main.c
@@ -5,28 +5,15 @@
 
 
 //funkcia na vypis pola, vypise z neho "dlzka" prvkov
-int print_pole( int p[], int dlzka, int x)
+int print_pole( int a[], int b[], int dlzka)
 {
-    int i;
-    int j=0;
-    for (i = 0; i <=dlzka; i++){
-        if(p[i]==x){
-
-            break;
-        }
-        if(i==dlzka){
-            p[i]=x;
-            dlzka=dlzka+1;
-        }
+    int suma=0;
+    for (int i = 0; i <=dlzka; i++){
+       suma =suma + (a[i]*b[i]);
     }
-    ;
-
-    for (i = 0; i <dlzka; i++){
-        printf("%d ", p[i]);
 
 
-    }
-    return dlzka+1;
+    return suma;
 }
 
 //funkcia naplni pole postupnostou cisel
@@ -40,11 +27,12 @@ void napln(int p[], int dlzka)
 int main()
 {
     int pole[MAX] = {0};
-    int prvocisla[] = {2,-5,8,-9};
+    int prvocisla[] = {1,3,-4};
+    int prvocisla1[] = {4,-2,-1};
     int dlzka = sizeof(prvocisla)/sizeof(prvocisla[0]);
     int i=0;
-    int novy_pocet_p=print_pole(prvocisla,dlzka,-5);
-
+    int novy_pocet_p=print_pole(prvocisla,prvocisla1,3);
+    printf("%d", novy_pocet_p);
 
 
 
